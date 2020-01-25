a193c0a1d3159dd31465627e3951422933ba7975
david-mateogit <davidmateo@outlook.com>
2020-01-25 10:13:56 -0400

    db connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Contact Keeper API..." })
